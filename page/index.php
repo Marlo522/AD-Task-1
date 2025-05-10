73a4a4b6fb4ed8729b3e17c452694a197ef2937b
Marlo522 <202311932@fit.edu.ph>
2025-05-10 11:37:07 +0800

    feat (genre page): add genres button, add an array that stores titles and images. and import images

--- page/index.php
+++ page/index.php
@@ -8,6 +8,47 @@
 </head>
 <body>
 
+    <?php
+// Array containing genres and Kdramas
+$kdramas = [
+    "Action" => [
+        ["title" => "Vagabond", "image" => "vagabond.jpg"],
+        ["title" => "Weak Hero Class 2", "image" => "weak_hero_class.jpg"],
+        ["title" => "Black Knight", "image" => "black_knight.jpg"],
+        ["title" => "Study Group", "image" => "study_group.jpg"],
+        ["title" => "Bloodhounds", "image" => "bloodhounds.jpg"]
+    ],
+    "Romance" => [
+        ["title" => "Crash Landing on You", "image" => "cloy.jpg"],
+        ["title" => "True Beauty", "image" => "true_beauty.jpg"],
+        ["title" => "Backstreet Rookie", "image" => "backstreet_rookie.jpg"],
+        ["title" => "What's Wrong with Secretary Kim", "image" => "secretary_kim.jpg"],
+        ["title" => "Hometown Cha-Cha-Cha", "image" => "hometown_chachacha.jpg"]
+    ],
+    "Medical" => [
+        ["title" => "Hospital Playlist", "image" => "hospital_playlist.jpg"],
+        ["title" => "Dr. Romantic", "image" => "dr_romantic.jpg"],
+        ["title" => "Doctor Stranger", "image" => "doctor_stranger.jpg"],
+        ["title" => "Resident Playbook", "image" => "resident_playbook.jpg"],
+        ["title" => "Trauma Code", "image" => "trauma_code.jpg"]
+    ],
+    "Comedy" => [
+        ["title" => "Welcome to Waikiki", "image" => "welcome_waikiki.jpg"],
+        ["title" => "Strong Woman Do Bong Soon", "image" => "strong_woman_dobongsoon.jpg"],
+        ["title" => "Chicken Nugget", "image" => "chicken_nugget.jpg"],
+        ["title" => "Prison Playbook", "image" => "prison_playbook.jpg"],
+        ["title" => "Gaus Electronics", "image" => "gaus_electronics.jpg"]
+    ],
+    "Thriller" => [
+        ["title" => "Happiness", "image" => "happiness.jpg"],
+        ["title" => "Sweet Home", "image" => "sweet_home.jpg"],
+        ["title" => "Taxi Driver", "image" => "taxi_driver.jpg"],
+        ["title" => "D.P", "image" => "dp.jpg"],
+        ["title" => "Vincenzo", "image" => "Vincenzo.jpg"]
+    ]
+];
+?>
+
     <header class="header">
     <nav>
         <ul class="nav-links">
@@ -26,6 +67,13 @@
         ?>
     </div>
 </header>
+
+<div class="genres">
+    <button onclick="filterKdramas('All')">All</button>
+    <?php foreach (array_keys($kdramas) as $genre): ?>
+        <button onclick="filterKdramas('<?php echo $genre; ?>')"><?php echo $genre; ?></button>
+    <?php endforeach; ?>
+</div>
 
 </body>
 </html>
