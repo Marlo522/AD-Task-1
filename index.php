f11e2a61c9111d156be01a96fbcbeaa56cd24bff
Marlo522 <202311932@fit.edu.ph>
2025-05-10 10:21:34 +0800

    feat (landing page): add bootstrap carousel and add images

--- index.php
+++ index.php
@@ -16,5 +16,41 @@
             </ul>
         </nav>
     </header>
+
+    <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/assets/img/black_knight.jpg" class="d-block w-100" alt="Black Knight">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Black Knight</h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/img/resident_playbook.jpg" class="d-block w-100" alt="ResidentPlaybook">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>ResidentPlaybook</h5>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/img/undercover_highschool.jpg" class="d-block w-100" alt="UnderCoverHighschool">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Undercover High School</h5>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </body>
 </html>
