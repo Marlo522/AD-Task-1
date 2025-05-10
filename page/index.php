818f29fe6e443bc7a395e780a9003c5b341a7a25
Marlo522 <202311932@fit.edu.ph>
2025-05-10 11:28:37 +0800

    feat (Genres page): add initial HTML structure and header for the Kdrama index page which is genres page

---
+++ page/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Kdrama</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+
+    <header class="header">
+    <nav>
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="Kdrama.php">Kdramas</a></li>
+        </ul>
+    </nav>
+    <div class="day">
+        <?php
+        $day = date('l');
+        if ($day) {
+            echo "<p>Today is: $day</p>";
+        } else {
+            echo "<p>Welcome!</p>";
+        }
+        ?>
+    </div>
+</header>
+
+</body>
+</html>
